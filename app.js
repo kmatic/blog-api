9c70309c28a4e3dcf3ab640aa3c8cca62ce732eb
Kristopher <kristopher_matic@hotmail.com>
2022-10-28 12:31:54 -0700

    file structure and file setup (app.js)

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
+const helmet = require('helmet');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// passport stuff
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JWTStrategy = require('passport-jwt').Strategy;
 
-var app = express();
+// .env
+require('dotenv').config();
 
+// import routers
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
+
+// instantiate app
+const app = express();
+
+// db setup 
+const mongoDB = process.env.MONGODB_URL;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+// middleware chain
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+app.use(helmet());
+app.use(cors());
 
+// routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// import controllers
+const commentController = require('../controllers/commentController');
+const postController = require('../controllers/postController');
+const userController = require('../controllers/userController');
+
+router.get
+
+module.exports = router;
