4c9fac91481675a692c2261c4e33bb900e8f83e3
Kristopher <kristopher_matic@hotmail.com>
2022-10-28 13:51:56 -0700

    router setup

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,55 @@ const commentController = require('../controllers/commentController');
 const postController = require('../controllers/postController');
 const userController = require('../controllers/userController');
 
-router.get
+// get / redirect to /posts 
+router.get('/', (req, res) => {
+    res.redirect('/api/posts');
+});
+
+// Map to CRUD 
+
+// STILL NEEDS AUTH MIDDLEWARE
+
+//POSTS
+
+// create a post
+router.post('/posts/', postController.createPost);
+
+// get all posts
+router.get('/posts', postController.getPosts);
+
+// get specific post
+router.get('/posts/:postid', postController.getPost);
+
+// update specific post 
+router.put('/posts/:postid', postController.updatePost);
+
+// delete specific post
+router.delete('/posts/:postid', postController.deletePost);
+
+// COMMENTS
+
+// create a comment on a post
+router.post('/posts/:postid/comments', commentController.createComment);
+
+// get all comments on a post
+router.get('/posts/:postid/comments', commentController.getComments);
+
+// get a specific comment on a post
+router.get('/posts/:postid/comments/:commentid', commentController.getComment);
+
+// update a comment 
+router.put('/posts/:postid/comments/:commentid', commentController.updateComment);
+
+// delete a comment
+router.delete('/posts/:postid/comments/:commentid', commentController.deleteComment);
+
+// USER
+
+// login user
+router.post('/login', userController.login);
+
+// logout user
+router.post('/logout', userController.logout);
 
 module.exports = router;
